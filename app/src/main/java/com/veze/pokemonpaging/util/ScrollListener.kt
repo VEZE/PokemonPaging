767195c0b14ae2e74e7f4809e56e6dcfe5b0ed59
Yury_Starzhynski <yura3882633@gmail.com>
2021-10-20 16:34:14 +0200

    loading new portion when list<screen

--- app/src/main/java/com/veze/pokemonpaging/util/ScrollListener.kt
+++ app/src/main/java/com/veze/pokemonpaging/util/ScrollListener.kt
@@ -1,9 +1,21 @@
 package com.veze.pokemonpaging.util
 
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class ScrollListener(private val pagingListener: PagingListener) :
     RecyclerView.OnScrollListener() {
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+        val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+        val lastItem = recyclerView.adapter?.itemCount ?: return
+
+        val lastVisibleItem = layoutManager.findLastVisibleItemPosition()
+
+        if (lastVisibleItem == lastItem - 1) {
+            pagingListener.onNextPage(recyclerView.adapter!!.itemCount)
+        }
+    }
 
     override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
         super.onScrollStateChanged(recyclerView, newState)
