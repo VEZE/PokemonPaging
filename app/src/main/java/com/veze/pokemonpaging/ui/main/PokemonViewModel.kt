0115c2ae0df13d8b8251741075ee56d099749b68
Yury_Starzhynski <yura3882633@gmail.com>
2021-11-08 10:13:23 +0100

    Added ViewModel and Factory to provide Presenter

---
+++ app/src/main/java/com/veze/pokemonpaging/ui/main/PokemonViewModel.kt
@@ -0,0 +1,7 @@
+package com.veze.pokemonpaging.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class PokemonViewModel(val presenter: PokemonPresenter) : ViewModel() {
+
+}
---
+++ app/src/main/java/com/veze/pokemonpaging/ui/main/ViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.veze.pokemonpaging.ui.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.xml.parsers.FactoryConfigurationError
+
+class ViewModelFactory() : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when (modelClass) {
+            PokemonViewModel::class.java -> {
+                return PokemonViewModel(PokemonPresenter(PokemonInteractor())) as T
+            }
+            else -> {
+                throw FactoryConfigurationError("Those factory not exist = $modelClass")
+            }
+        }
+    }
+}
