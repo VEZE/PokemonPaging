f36ecd55f86cc0b917b4055c238af937d4b50d95
Yury_Starzhynski <yura3882633@gmail.com>
2021-11-08 10:12:24 +0100

    Added LoadingAdapter [refactored States]

--- app/src/main/java/com/veze/pokemonpaging/ui/main/LoadingAdapter.kt
+++ app/src/main/java/com/veze/pokemonpaging/ui/main/LoadingAdapter.kt
@@ -1,45 +1,82 @@
 package com.veze.pokemonpaging.ui.main
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.veze.pokemonpaging.R
+import com.veze.pokemonpaging.databinding.LoadingBinding
 
-class LoadingAdapter : RecyclerView.Adapter<LoadingAdapter.LoadingViewHolder>() {
+class LoadingAdapter : RecyclerView.Adapter<FooterStateViewHolder>() {
 
     var loadState: LoadState = LoadState.Done
-        set(value) {
-            when (field) {
-                LoadState.Done -> notifyItemChanged(0)
-                LoadState.Loading -> notifyItemChanged(0)
+        set(loadState) {
+            if (field != loadState) {
+
+                when {
+                    field is LoadState.Done -> notifyItemInserted(0)
+                    loadState is LoadState.Done -> notifyItemRemoved(0)
+                    else -> notifyItemChanged(0)
+                }
+
+                field = loadState
             }
-            field = value
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LoadingViewHolder =
-        LoadingViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.loading, parent, false)
-        )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FooterStateViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.loading, parent, false)
 
-    override fun onBindViewHolder(holder: LoadingViewHolder, position: Int) {
-        holder.bind(loadState)
+        val binding = LoadingBinding.bind(view)
+
+        return FooterStateViewHolder(binding) {}
     }
 
-    override fun getItemCount(): Int = 1
+    override fun onBindViewHolder(holder: FooterStateViewHolder, position: Int) {
+        holder.bind(loadState)
 
-    class LoadingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    }
 
-        fun bind(loadState: LoadState) = with(itemView) {
-            val progress = findViewById<TextView>(R.id.loading)
-            progress.isVisible = LoadState.Loading == loadState
+    override fun getItemCount(): Int =
+        if (loadState is LoadState.Loading || loadState is LoadState.Error) 1 else 0
+
+}
+
+
+class FooterStateViewHolder(private val binding: LoadingBinding, retry: () -> Unit) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    init {
+        binding.retryButton.apply {
+            setOnClickListener { retry.invoke() }
+        }
+    }
+
+    fun bind(loadState: LoadState) {
+        if (loadState is LoadState.Error) {
+            binding.retryText.text = loadState.error.message
+        }
+
+        binding.retry.isVisible = loadState is LoadState.Error
+        binding.loading.isVisible = loadState is LoadState.Loading
+
+    }
+
+    companion object {
+        fun create(parent: ViewGroup, retry: () -> Unit): FooterStateViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.loading, parent, false)
+
+            val binding = LoadingBinding.bind(view)
+
+            return FooterStateViewHolder(binding, retry)
         }
     }
 }
 
+
 sealed class LoadState {
     object Loading : LoadState()
+    data class Error(val error: Throwable) : LoadState()
     object Done : LoadState()
 }
